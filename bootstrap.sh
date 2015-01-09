8b6a3f529bb0a1ea1be2dec56d5d5b08c33efa6b
evilGmonkey <freddie@uhurunet.com>
2015-01-09 12:31:29 +0300

    Still lots of errors, and there are two config files which must be wrong, nginx.conf and owncloud.conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-touch /var/log/nginx/access.log
-touch /var/log/nginx/error.log
-touch /var/log/cron/owncloud.log
+#touch /var/log/nginx/access.log
+#touch /var/log/nginx/error.log
+#touch /var/log/cron/owncloud.log
 
 if [ -z "$SSL_CERT" ]; then
     echo "\nCopying nginx.conf without SSL support..\n"
@@ -13,11 +13,12 @@ else
     sed -i "s#-x-replace-key-x-#$SSL_KEY#" /root/nginx_ssl.conf
     cp /root/nginx_ssl.conf /etc/nginx/nginx.conf
 fi
-chown -R www-data:www-data /var/www/owncloud /owncloud
+chown -R nginx:nginx /var/www/owncloud /owncloud
 echo "Starting server..\n"
 
 tail -F /var/log/nginx/*.log /var/log/cron/owncloud.log &
 
-/usr/sbin/cron -f &
-/usr/sbin/php5-fpm
+#/usr/sbin/crond -f &
+#/usr/sbin/php5-fpm
 /usr/sbin/nginx
+/bin/bash
